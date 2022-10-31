019c3354277a642169dc3dcfec6aa177e9723588
Gregtstu <boltnev.grisha@yandex.ru>
2022-10-31 17:49:32 +0300

    added project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { CreatePageComponent } from './pages/create-page/create-page.component';
+import { UpdatePageComponent } from './pages/update-page/update-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    CreatePageComponent,
+    UpdatePageComponent
   ],
   imports: [
     BrowserModule,
